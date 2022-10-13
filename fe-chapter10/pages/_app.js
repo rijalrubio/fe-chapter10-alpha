2e16d30ae17c9063667fa674e1d7814225b3b496
rijalrubio <rijalrubio@gmail.com>
2022-10-13 19:18:04 +0700

    fixing navbar

--- fe-chapter10/pages/_app.js
+++ fe-chapter10/pages/_app.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import "../styles/globals.css";
 import Header from "../components/header/headers";
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
-=======
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
@@ -20,10 +10,10 @@ config.autoAddCss = false
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Header/>
       <Component {...pageProps} />
     </Provider>
   )
->>>>>>> 9f63896d9f47a97cc92cbeb0cad7f55e289d25fe
 }
 
 export default MyApp;
