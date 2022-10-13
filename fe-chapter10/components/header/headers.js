a2f496516fa26afb17a6b3e64f36dfb31debf29d
rijalrubio <rijalrubio@gmail.com>
2022-10-13 19:25:04 +0700

    fixing navbar

--- fe-chapter10/components/header/headers.js
+++ fe-chapter10/components/header/headers.js
@@ -131,7 +131,7 @@ export default function Header() {
                           <Menu.Item>
                             {({ active }) => (
                               <a
-                                href="login"
+                                href="/"
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
                                   "block px-4 py-2 text-sm text-gray-700"
