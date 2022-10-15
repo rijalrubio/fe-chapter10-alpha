a9cd2f7c1a3569040b9a825ed055689246f49c7c
mikaelkadga <mikaelkadga@gmail.com>
2022-10-15 13:50:13 +0700

    register and update

--- fe-chapter10/share/redux/store.js
+++ fe-chapter10/share/redux/store.js
@@ -1,9 +1,11 @@
 import authSlice from './authSlice';
+import registerSlice from './registerSlice';
 
 const { configureStore } = require('@reduxjs/toolkit');
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    register: registerSlice
   },
 });
